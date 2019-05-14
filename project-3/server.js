cff80e42eff183c69781ff5a314e5d8993fe442a
Indra Nallappan <back2indra@gmail.com>
2019-05-14 00:58:39 -0500

    backend fix

--- project-3/server.js
+++ project-3/server.js
@@ -4,11 +4,12 @@ var path = require('path');
 var app = express();
 var db = require("./models");
 var bodyParser = require('body-parser');
-var expressValidator = require('express-validator');
-var flash = require('connect-flash');
+// var expressValidator = require('express-validator');
+// var flash = require('connect-flash');
 var session = require('express-session');
 var passport = require('passport');
 var routes = require("./routes")
+
 require('dotenv').config();
 
 var PORT = process.env.PORT || 3001;
@@ -27,8 +28,6 @@ app.use(session({
   cookie: { secure: false }
 }));
 
-
-
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -42,41 +41,41 @@ if (process.env.NODE_ENV === "production") {
 
 
 
-// Express Messages Middleware
-app.use(flash());
-app.use(function (req, res, next) {
-  res.locals.message = req.flash('message');
-  res.locals.lmessage = req.flash('lmessage');
-  //res.locals.lmessage = req.flash('lmessage');
-  next();
-});
-app.use(routes);
+// // Express Messages Middleware
+// app.use(flash());
+// app.use(function (req, res, next) {
+//   res.locals.message = req.flash('message');
+//   res.locals.lmessage = req.flash('lmessage');
+//   //res.locals.lmessage = req.flash('lmessage');
+//   next();
+// });
+app.use('/', routes)
 // Express Validator Middleware
-app.use(expressValidator({
-  errorFormatter: function (param, msg, value) {
-    var namespace = param.split('.')
-      , root = namespace.shift()
-      , formParam = root;
+// app.use(expressValidator({
+//   errorFormatter: function (param, msg, value) {
+//     var namespace = param.split('.')
+//       , root = namespace.shift()
+//       , formParam = root;
 
-    while (namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
-    }
-    return {
-      param: formParam,
-      msg: msg,
-      value: value
-    };
-  }
-}));
+//     while (namespace.length) {
+//       formParam += '[' + namespace.shift() + ']';
+//     }
+//     return {
+//       param: formParam,
+//       msg: msg,
+//       value: value
+//     };
+//   }
+// }));
 
 // Passport Config
-require('./passport/index')(passport, db.User);
+//require('./passport/index')(passport, db.User);
 
 // Define API routes here
-require('./routes/api/users.js')(app, passport);
+//require('./routes/api/users.js')(app, passport);
 // require('./routes/api/cars')
 
-var syncOptions = { force: false }
+//var syncOptions = { force: false }
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
